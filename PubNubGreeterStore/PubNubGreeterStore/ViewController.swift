60d7c899b6a9496f8091500ee0a8105c37e359c1
ertheis <ertheis@Erics-MacBook-Pro.local>
2014-08-05 11:20:04 -0700

    fixed issues due to swift beta 5

--- PubNubGreeterStore/PubNubGreeterStore/ViewController.swift
+++ PubNubGreeterStore/PubNubGreeterStore/ViewController.swift
@@ -16,9 +16,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet var status: UILabel!
     var beaconText = "Loading"
     
-    var defaultData: [NSDictionary] = [["textLabel":"Cutomers currently in the store will appear here.", "detailTextLabel":"iBeacon broadcast has started with Major: 9, Minor: 6.", "imgPath":"./DefaultPic"]]
+    let defaultData = ["textLabel":"Cutomers currently in the store will appear here.", "detailTextLabel":"iBeacon broadcast has started with Major: 9, Minor: 6.", "imgPath":"./DefaultPic"]
     var tableData:PNObject = PNObject()
-    var changeData = [String]()
+    var changeData:[String] = []
     
     let uuidObj = NSUUID(UUIDString: "0CF052C2-97CA-407C-84F8-B62AAC4E9020")
     
@@ -31,7 +31,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
         
         self.region = CLBeaconRegion(proximityUUID: uuidObj, major: 9, minor: 6, identifier: "com.pubnub.test")
-        self.beaconData = self.region.peripheralDataWithMeasuredPower(nil)
+        beaconData = self.region.peripheralDataWithMeasuredPower(nil) as NSDictionary
+        println(beaconData.allKeys)
+        println(beaconData.allValues)
         self.manager = CBPeripheralManager(delegate: self, queue: nil)
         
         let myConfig = PNConfiguration(forOrigin: "pubsub-beta.pubnub.com", publishKey: appDelegate.pubKey, subscribeKey: appDelegate.subKey, secretKey: nil, authorizationKey: appDelegate.authKey)
@@ -108,40 +110,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel.numberOfLines = 0
         cell.imageView.layer.borderColor = UIColor.whiteColor().CGColor
         
-        var cellData: NSDictionary
+        var cellData: Dictionary<String, String>
         if tableData.count < 1 {
-            cellData = defaultData[indexPath.row]
+            cellData = self.defaultData
         } else {
-            cellData = tableData.allValues[indexPath.row] as NSDictionary
-            for index in 0...changeData.count-1 {
-                if tableData.allKeys[indexPath.row] as String == changeData[index] {
-                    cell.backgroundColor = UIColor(red: 206.0/255.0, green: 17/255.0, blue: 38/255.0, alpha: 1)
-                    cell.textLabel.textColor = UIColor.whiteColor()
-                    cell.detailTextLabel.textColor = UIColor.whiteColor()
-                    cell.imageView.layer.borderColor = UIColor(red: 206.0/255.0, green: 17/255.0, blue: 38/255.0, alpha: 1).CGColor
-                    let delay = 1.5 * Double(NSEC_PER_SEC)
-                    let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-                    dispatch_after(time, dispatch_get_main_queue()) {
-                        cell.backgroundColor = UIColor.whiteColor()
-                        cell.textLabel.textColor = UIColor.blackColor()
-                        cell.detailTextLabel.textColor = UIColor.blackColor()
-                        cell.imageView.layer.borderColor = UIColor.whiteColor().CGColor
+            cellData = tableData.allValues[indexPath.row] as Dictionary
+            if !changeData.isEmpty {
+                for index in 0...changeData.count-1 {
+                    if tableData.allKeys[indexPath.row] as String == changeData[index] {
+                        cell.backgroundColor = UIColor(red: 206.0/255.0, green: 17/255.0, blue: 38/255.0, alpha: 1)
+                        cell.textLabel.textColor = UIColor.whiteColor()
+                        cell.detailTextLabel.textColor = UIColor.whiteColor()
+                        cell.imageView.layer.borderColor = UIColor(red: 206.0/255.0, green: 17/255.0, blue: 38/255.0, alpha: 1).CGColor
+                        let delay = 1.5 * Double(NSEC_PER_SEC)
+                        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+                        dispatch_after(time, dispatch_get_main_queue()) {
+                            cell.backgroundColor = UIColor.whiteColor()
+                            cell.textLabel.textColor = UIColor.blackColor()
+                            cell.detailTextLabel.textColor = UIColor.blackColor()
+                            cell.imageView.layer.borderColor = UIColor.whiteColor().CGColor
+                        }
+                        changeData.removeAtIndex(index)
+                        break
                     }
-                    changeData.removeAtIndex(index)
-                    break
                 }
             }
         }
         
-        cell.textLabel.text = cellData.objectForKey("textLabel") as String
-        cell.detailTextLabel.text = cellData.objectForKey("detailTextLabel") as String
+        cell.textLabel.text = cellData["textLabel"]
+        cell.detailTextLabel.text = cellData["detailTextLabel"]
         var theImage = UIImage()
-        if cellData.objectForKey("imgPath") as String == "./DefaultPic" {
-            let path = NSBundle.mainBundle().pathForResource(cellData.objectForKey("imgPath") as String, ofType: "png")
+        if cellData["imgPath"] == "./DefaultPic" {
+            let path = NSBundle.mainBundle().pathForResource(cellData["imgPath"], ofType: "png")
             theImage = UIImage(contentsOfFile: path)
         } else {
-            var raw = cellData.objectForKey("imgPath") as String
-            theImage = UIImage(data: NSData(base64EncodedString: raw, options: NSDataBase64DecodingOptions.fromRaw(0)!))
+            var raw = cellData["imgPath"] as String?
+            theImage = UIImage(data: NSData(base64EncodedString: raw!, options: NSDataBase64DecodingOptions.fromRaw(0)!))
         }
         cell.imageView.image = theImage
         cell.imageView.layer.cornerRadius = 40
